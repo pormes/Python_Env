034720af62082f8160a9967310d6d6aebe15b46c
Pormes <66345108+pormes@users.noreply.github.com>
2020-08-29 08:02:33 +0700

    Update and rename library.sh to env.sh

---
+++ env.sh
@@ -0,0 +1,41 @@
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "ARE YOU READY FOR START NOW"
+echo "LET'S SUBSCRIBE PORMES17"
+echo "WAITING FOR STARTED"
+echo "LIBRARY LENGKAP PYTHON ENV"
+echo "WITH 6 BASH ACTIVATOR PRO"
+echo "DUDUK YANG MANIS DAN AKAN SEGERA KITA MULAI"
+
+pkg install python2 -y
+pkg install python -y
+pip install --upgrade pip setuptools
+pip install pipfile
+pip install virtualenv
+pip install virtualenv-clone
+pip install virtualenvwrapper
+pip install pew
+pip install tox
+pip install nox
+pip install bar
+pip install pipenv
+pipenv install --dev --skip-lock
+pipenv shell
+
+echo "ALL DEPENDENCIES LIBRARY OF PACKAGING IS FINISHED"
+echo "DON'T FORGET TO SUBSCRIBE PORMES17"
+echo ""
+echo ""
+echo ""
+echo "ARE YOU PRO NOW"
+echo "SUPPORT ME AND SUBSCRIBE PORMES17"
+echo "JIKA ANDA MERASA BERMANFAAT JANGAN SUNGKAN UNTUK CLICK TOMBOL SUBSCRIBE"
+echo "CODED BY PORMES17"
+exit
